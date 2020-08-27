d86b7b3f4a78ef7b9f73182c901f9285ca3d41ca
benz-e-class <benz-e-class@kakao.com>
2020-08-27 16:41:02 +0900

    first commit

---
+++ src/main/java/io/playdata/themorethebetter/TheMoreTheBetterApplication.java
@@ -0,0 +1,13 @@
+package io.playdata.themorethebetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMoreTheBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMoreTheBetterApplication.class, args);
+	}
+
+}
