5766d962d6c363631d40f02d80bf620fd3da302b
chaeyeonp <chaeyeon.park@outlook.com>
2020-09-05 22:18:06 +0900

    Merge branch parkjunsoo

--- src/main.js
+++ src/main.js
@@ -8,13 +8,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import naver from 'vue-naver-maps';
 
 Vue.use(BootstrapVue)
 Vue.use(Vuetify);
 Vue.config.productionTip = false
-
-
-
+Vue.use(naver, {
+  clientID: '8c5g8vyz8e',
+  useGovAPI: true, //공공 클라우드 API 사용 (선택)
+  subModules:'' // 서브모듈 (선택)
+});
 new Vue({
   vuetify: new Vuetify(),
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../components/HelloWorld'
-import Login from '../components/Login.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from '../components/Login.vue';
 import Main from "@/components/Main";
-import Board from "@/components/IndexWaitingList.vue"
-import Register from '@/components/Member'
+import Board from "@/components/IndexWaitingList.vue";
+import Enroll from "@/views/Enroll.vue";
 
 Vue.use(VueRouter)
-
-
-
   const routes = [
   {
     path: '/',
     name: 'Home',
     component: Main,
-    template: Main
-  }, {
-      path:'/board',
-      name:'Main',
-      component: Board,
-      template:Board
-
-    }, {
+  }, 
+  {
+    path:'/board',
+    name:'Main',
+    component: Board,
+  },
+     {
     path: '/login',
     name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: Login,
-    template: Login
- }, {
-          path: '/reg',
-          name: 'Register',
-          component: Register,
-          template: Register
-      }
+ },
+ {
+   path: '/enroll',
+   name: 'Enroll',
+   component: Enroll
+ }
 ]
 
 const router = new VueRouter({
