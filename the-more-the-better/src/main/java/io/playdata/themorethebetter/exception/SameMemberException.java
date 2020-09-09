226b9adaca774c5fc05923e0c0c1b914c07f3c0d
j00hyun <loove1997@naver.com>
2020-09-09 16:46:54 +0900

    before merge

--- the-more-the-better/src/main/java/io/playdata/themorethebetter/exception/SameMemberException.java
+++
@@ -1,9 +0,0 @@
-package io.playdata.themorethebetter.exception;
-
-public class SameMemberException extends RuntimeException
-{
-	public SameMemberException() 
-	{
-		super("이미 존재하는 회원입니다!");
-	}
-}
