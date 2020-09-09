785798650cf303f7700cff5178b80f9bcab02a4a
junsoo <01junsoo@gmail.com>
2020-09-09 16:34:09 +0900

    merge plz

--- the-more-the-better/src/main/java/io/playdata/themorethebetter/exception/ForbiddenException.java
+++
@@ -1,7 +0,0 @@
-package io.playdata.themorethebetter.exception;
-
-public class ForbiddenException extends RuntimeException {
-	public ForbiddenException(String str) {
-		super(str);
-	}
-}
--- the-more-the-better/src/main/java/io/playdata/themorethebetter/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package io.playdata.themorethebetter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.playdata.themorethebetter.domain.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-	
-}
